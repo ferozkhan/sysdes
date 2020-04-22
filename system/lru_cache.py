26a635914c6662b3bbe16143a7a415cf4f8183c6
feroz khan <ferozkhan27@gmail.com>
2020-04-23 01:50:06 +0200

    init lru cache

---
+++ system/lru_cache.py
@@ -0,0 +1,19 @@
+
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        pass
+
+    def put(self, key: int, val: int) -> None:
+        pass
